20adcfd478e8920a8d0cb24f8aa1dfb78792c8a3
maoge <ultravirs@outlook.com>
2023-11-14 17:01:00 +0800

    remove Cargo.lock

--- zz_metasvr/src/proto/global_types.rs
+++ zz_metasvr/src/proto/global_types.rs
@@ -35,33 +35,33 @@ pub struct RetResponse {
 }
     
 impl Responder for RetResponse {
-        type Error = Error;
-        type Future = Ready<Result<HttpResponse, Error>>;
-    
-        fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-            let body = serde_json::to_string(&self).unwrap();
-    
-            // Create response and set content type
-            ready(Ok(HttpResponse::Ok()
-                .content_type("application/json")
-                .body(body)))
-        }
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
 }
     
 impl RetResponse {
-        pub fn new() -> Self {
-            Self { ret_code: CODE_NOK, ret_info: "".to_string(), magic_key: "".to_string(), }
-        }
+    pub fn new() -> Self {
+        Self { ret_code: CODE_NOK, ret_info: "".to_string(), magic_key: "".to_string(), }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ZZAccount {
-        pub acc_id: String,
-        pub acc_name: String,
-        pub phone_num: String,
-        pub mail: String,
-        pub passwd: String,
-        pub create_time: i64,
+    pub acc_id: String,
+    pub acc_name: String,
+    pub phone_num: String,
+    pub mail: String,
+    pub passwd: String,
+    pub create_time: i64,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
